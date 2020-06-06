2e86b5fad211281c6d6bf9d39932ff27577f8b0c
AmandaMichael259 <michael.amanda259@gmail.com>
2020-06-07 00:23:05 +0530

    Check RegisterActivity.java

--- app/src/main/java/com/example/baby/RegisterActivity.java
+++ app/src/main/java/com/example/baby/RegisterActivity.java
@@ -1,97 +1,74 @@
 package com.example.baby;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextInputLayout name , userName, userPhoneNumber, userEmail, userPassword;
+    Button regButton , loginButton;
 
-   /* private TextInputLayout userName, userPassword, userEmail;
-    private Button regButton;
-    private TextView userLogin;
-    private FirebaseAuth firebaseAuth;*/
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
-
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        /*setupUIView();
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.etName);
+        userName = findViewById(R.id.etUserName);
+        userEmail =  findViewById(R.id.etUserEmail);
+        userPhoneNumber = findViewById(R.id.etPhoneNumber);
+        userPassword = findViewById(R.id.etUserPassword);
+
+        regButton = findViewById(R.id.btReg);
+        loginButton =  findViewById(R.id.btLogin);
+
+        //firebaseAuth = FirebaseAuth.getInstance();
 
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validate()){
-                    //uploading data to DB
-                    String user_email = userEmail.getText().toString().trim();
-                    String user_password = userPassword.getText().toString().trim();
 
-                    firebaseAuth.createUserWithEmailAndPassword(user_email,user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                            Toast.makeText(RegisterActivity.this,"Successful",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
-                        }
-                        else {
-                                Toast.makeText(RegisterActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("user");
+                myRef.setValue("Hello, World!");
 
-                            }
-                        }
-                    });
-                }
+                //getting values
+                String Name = name.getEditText().getText().toString();
+                String UserName = userName.getEditText().getText().toString();
+                String email = userEmail.getEditText().getText().toString();
+                String phone = userPhoneNumber.getEditText().getText().toString();
+                String password = userPassword.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(Name,UserName,email,phone,password);
+
+               reference.child(phone).setValue(helperClass);
+               //reference.setValue("first data storage");
             }
         });
 
-        userLogin.setOnClickListener(new View.OnClickListener() {
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
             }
         });
     }
-    private void setupUIView()
-    {
-        userName = (EditText)findViewById(R.id.etUserName);
-        userPassword = (EditText)findViewById(R.id.etUserPassword);
-        userEmail = (EditText)findViewById(R.id.etUserEmail);
-        regButton = (Button)findViewById(R.id.btReg);
-        userLogin = (TextView)findViewById(R.id.tvUserLogin);
-    }
 
-    private Boolean validate()
-    {
-        Boolean result = false;
 
-        String name= userName.getText().toString();
-        String password= userPassword.getText().toString();
-        String email= userEmail.getText().toString();
-
-        if (name.isEmpty() && password.isEmpty() && email.isEmpty())
-        {
-            Toast.makeText(this,"Please Enter all details",Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            result = true;
-        }
-        return  result;*/
-    }
 }
